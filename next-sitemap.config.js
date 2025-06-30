13f60aa19ad0119879d0d721a7571294350f560f
santhoshkumarampolu <santhoshkuamrampolu@gmail.com>
2025-06-30 22:51:39 +0530

    feat: add sitemap and robots.txt configuration for SEO
    - Introduced next-sitemap for automatic sitemap generation.
    - Created sitemap.xml and sitemap-0.xml files to list site URLs.
    - Added robots.txt to manage search engine indexing.
    - Updated package.json to include next-sitemap in postbuild script.
    - Updated package-lock.json to include next-sitemap dependency.

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://aistorystudio.com',
+    generateRobotsTxt: true, // (optional) Generate a robots.txt file
+    // ...add more options if needed
+  };
